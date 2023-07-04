9d8f845fdb6321260c2fae9b5c43c5b30d1c96fa
brixxt27 <brixxt27@gmail.com>
2023-07-04 17:58:39 +0900

    feat: controller 생성
    nest g controller boards --no-spec 을 통해 다음과 같은 일이 일어난다.
    - nest cli는 boards 디렉토리를 찾는다.
    - boards.controller.ts 파일을 생성한다.
    - boards 디렉토리 내에 module 파일을 찾는다.
    - module 파일에 controller 를 import 한다.

--- nestjs-board-app/src/boards/boards.module.ts
+++ nestjs-board-app/src/boards/boards.module.ts
@@ -1,4 +1,9 @@
+//  module 을 생성할 때 직접 추가하는 것이 아닌 nest cli 를 사용한다.
+//  nest g module boards 를 실행하니 알아서 boards 디렉토리가 생성되고, ts 파일이 생기며, app.module.ts 파일에 자동으로 import {BoardsModule} from './boards/boards.module'; 과 데코레이터의 import 에 BoardsModule 이 추가된다.
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
 
-@Module({})
+@Module({
+  controllers: [BoardsController]
+})
 export class BoardsModule {}
