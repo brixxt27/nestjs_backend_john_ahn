61ab2bd7ac182982eea5d5bd1710766971523570
brixxt27 <brixxt27@gmail.com>
2023-07-04 21:19:42 +0900

    feat: service를 구현하여 사용해봄
    - boards 모듈에서 provider 와 controller 를 설정해두고 provider 는
    서비스의 기능을 controller에게 의존성을 주입해주어서 사용할 수 있게 함.
    - boards 모듈의 @Get의 엔드포인트를 boards로 따로 주면
      localhost:3000/boards 가 아닌 localhost:3000/boards/boards 로 요청을
    줘야 반응을 하게 된다.

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  
+  // 프로퍼티의 이름: 프로퍼티의 타입
+  // boardsService: BoardsService;
+  
+  // constructor(boardsService: BoardsService) {
+  //   this.boardsService = boardsService;
+  // }
+
+  // 위 네 줄을 한 줄로 줄일 수 있음.
+  // 생성자 내에 private 를 작성하면 암묵적으로 해당 클래스의 프로퍼티가 됨.
+  constructor(private boardsService: BoardsService) {}
+
+  @Get() // boards 모듈의 /boards이라서 https://localhost:3000/boards/borads여야 작동한다.
+  getHello(): string {
+    return this.boardsService.getHello();
+  }
+}
--- nestjs-board-app/src/boards/boards.module.ts
+++ nestjs-board-app/src/boards/boards.module.ts
@@ -2,8 +2,10 @@
 //  nest g module boards 를 실행하니 알아서 boards 디렉토리가 생성되고, ts 파일이 생기며, app.module.ts 파일에 자동으로 import {BoardsModule} from './boards/boards.module'; 과 데코레이터의 import 에 BoardsModule 이 추가된다.
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
 @Module({
-  controllers: [BoardsController]
+  controllers: [BoardsController],
+  providers: [BoardsService]
 })
 export class BoardsModule {}
