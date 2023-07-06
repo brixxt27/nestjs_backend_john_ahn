1fb381ccc4d2e6b2b30c2a9677ba18f79f36cd6f
brixxt27 <brixxt27@gmail.com>
2023-07-06 21:45:18 +0900

    feat: arrow function 공부 중
    배열의 find() 메소드는 배열 요소, 인덱스, 배열 자체를 인자로 받을 수
    있다.
    이 중 요소를 필수로 받아야 한다. 요소를 탐색하면서 인자로 받은
    콜백함수를 실행하여 true 값을 반환하는 요소를 반환한다. 없으면 undefined

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,22 +1,21 @@
-import { Controller,Get } from '@nestjs/common';
+import { Body, Controller,Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board';
 
 @Controller('boards')
 export class BoardsController {
-  
-  // 프로퍼티의 이름: 프로퍼티의 타입
-  // boardsService: BoardsService;
-  
-  // constructor(boardsService: BoardsService) {
-  //   this.boardsService = boardsService;
-  // }
-
-  // 위 네 줄을 한 줄로 줄일 수 있음.
-  // 생성자 내에 private 를 작성하면 암묵적으로 해당 클래스의 프로퍼티가 됨.
   constructor(private boardsService: BoardsService) {}
 
-  @Get() // boards 모듈의 /boards이라서 https://localhost:3000/boards/borads여야 작동한다.
-  getHello(): string {
-    return this.boardsService.getHello();
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto) {
+
+    return this.boardsService.createBoard(createBoardDto);
   }
 }
