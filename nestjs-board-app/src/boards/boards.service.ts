f573a9b4f2516009460210ba483934f41439f5ba
brixxt27 <brixxt27@gmail.com>
2023-07-24 20:31:36 +0900

    feat: entity 의 repository pattern 으로 getBoardById 구현
    board.service.ts
    - getBoardById 메소드를 기존에 프로세스 내의 메모리에서 가져오는
      방식에서 database 를 경유하는 방식으로 변경함. findOne 메소드로
      데이터를 가져 올 수 있다.
    - Type 'number' has no properties in common with type 'FindOneOptions<Board>'.ts(2559)
    - getBoardById 에서 number type 이 특정 properties 를 갖지 않는다라는
      에러 문구가 나왔다.
    - 이로써 사용을 잘못했나 싶었는데 board.repository.ts 파일에서 사용한
      @EntityRepository() 데코레이터를 더이상 사용하지 않는다는 경고 문구를
      보고 이것이 문제일 것이라 생각했다.

--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -3,10 +3,20 @@ import { BoardStatus } from './board-status.enum';
 import {v1 as uuid} from 'uuid';// uuid 의 버전 v1을 사용한다는 의미, as 를 사용하면 이름을 변경 가능하다.
 import { CreateBoardDto } from './dto/create-board';
 import { IsEmpty } from 'class-validator';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-
+  constructor( /* - service 를 controller 에 주입하는 행위는 Service class 위에 @Injectable() 데코레이터를 추가한다. 이에 반해 BoardRepository 타입에 해당하는 변수를 선언하며 의존성을 주입하고 싶다면 생성자의 인자에 @InjectRepository(<repository type>) 을 통해 해야 한다.
+  - client 로부터 온 request 는 파싱 되어 controller 에서 해당하는 경로로 routing 되게 된다. endpoint 에서 가장 앞쪽에 해당하는 리소스가 module 의 이름이 되며, 해당 이름을 가진 controller 와 service 로 만들면 가독성이 좋다. 
+  - controller 에서 비교적 복잡한 로직은 service 라는 class 로 관리 된다. service 는 module 에서 provider 라는 이름으로 controller 에 의존성을 주입 시킨다.
+  - Database 에 대한 로직은 service 에서 다루지 않고 복잡한 일은 service 에 분배하듯이, 또 다시 책임을 분배한다. 이는 Repository 라고 불리는 class 로 관리 된다.
+  */
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository
+  ) {}
   // getAllBoards(): Board[] {
   //   return this.boards;
   // }
@@ -28,6 +38,14 @@ export class BoardsService {
   //   return board;
   // }
 
+  async getBoardById(id: number): Promise <Board> {
+    const found = await this.boardRepository.findOne(id);
+
+    if (found === undefined) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return found;
+  }
   // getBoardById(id: string): Board {
   //   // find()의 인자로는 콜백함수가 들어간다. 콜백함수의 인자로는 배열의 요소, 배열의 인덱스, 배열 자체가 들어간다. 배열의 요소는 필수이고, 나머지는 선택이다. 배열의 요소란? 배열의 각 요소에 대해 순서대로 한 번씩 호출하고, predicate 가 true 를 반환하는 요소를 찾을 때까지 오름차순으로 호출한다. 이러한 요소가 발견되면 find 는 즉시 해당 요소 값을 반환한다. 그렇지 않으면 find 는 undefined 를 반환한다. predicate 가 true 인 배열의 첫 번째 요소의 값을 반환하고, 그렇지 않으면 undefined 를 반환한다.
   //   const found = this.boards.find((board) => (board.id === id));
